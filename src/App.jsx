bdba587e5672060bc69577c9f306118f9eea9583
Gopal Tade <gopaltade17@gmail.com>
2024-02-27 23:33:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Quiz from "./Component/Quiz"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Quiz />
+    </>
+  )
+}
+
+export default App
